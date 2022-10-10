ca60b1888d0b783bb516ee29f10f3f0b51144dec
Gabriel Gomez <ggomez.box@gmail.com>
2022-10-10 10:39:52 +0200

    setup session config to use cookies and increase maxAge

--- app.js
+++ app.js
@@ -18,9 +18,6 @@ const app = express();
 // ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
 require("./config")(app);
 
-require('./config/session.config')(app);
-
-
 const capitalized = require("./utils/capitalized");
 const projectName = "filmgalaxy";
 
@@ -36,11 +33,6 @@ app.use("/", require("./routes/index.routes"));
 app.use("/", require("./routes/auth.routes"));
 app.use("/", require("./routes/festival.routes"));
 app.use("/", require("./routes/project.routes"));
-
-
-// const festivalSearch = require("./routes/festivalsearch.routes");
-// app.use("/", festivalSearch);
-
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
