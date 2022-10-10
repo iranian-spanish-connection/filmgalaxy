309bd82417decee4f8971c219db135ac590981b2
Mog Rouhi <moghadase.roohi@gmail.com>
2022-10-10 16:46:51 +0100

    fix festival page

--- routes/festival.routes.js
+++ routes/festival.routes.js
@@ -14,12 +14,57 @@ router.get("/festivals", (req, res, next) => {
   })
 })
 
+//   router.get("/profile/myfestival", (req, res) => {
+//     res.render("festivals/myfestival");
+// });
+
+    router.get("/profile/myfestival", (req, res) => {
+    console.log(req.session.user)
+    Festival.findOne({submitter: req.session.user._id })
+    .then((festivalFromDB)=>{
+      console.log(festivalFromDB)
+        if(festivalFromDB){
+          res.render("festivals/myfestival", {festivalFromDB});
+        } 
+    })
+    .catch(err=>{
+      console.log("Error getting my festival from db", err)
+    })
+  });
 
 
-router.get("/festivals/create", (req, res) => {
-  res.render("festivals/create");
+router.get("/profile/add-my-festival", (req, res) => {
+  res.render("festivals/createfestival");
 });
 
+router.post("/profile/myfestival", (req, res) => {
+  const myFestival = {
+    title: req.body.title,
+    submitter: req.session.user
+  }
+  Festival.create(myFestival)
+  .then((myFestival)=>{
+    res.render("festivals/myfestival", {myFestival});
+  })
+  .catch(err=>{
+    console.log("Error creating my festival", err)
+  })
+});
+
+   router.post("/profile/myfestival/remove", (req, res, next)=>{
+    console.log("something")
+   Festival.findOneAndDelete({submitter: req.session.user._id })
+  .then((result)=>{
+    console.log("something2")
+
+    console.log(result)
+    res.render("/profile/myfestival")
+  })
+  .catch(err=>{
+    console.log("Error deleting my festival", err)
+  })
+
+})
 
 
 
