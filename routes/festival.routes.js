8eef75cee3d880fccf32ce40b49ab57eb3cbbdd0
Gabriel Gomez <ggomez.box@gmail.com>
2022-10-10 17:59:33 +0200

    fix routes adding festival

--- routes/festival.routes.js
+++ routes/festival.routes.js
@@ -2,36 +2,30 @@ const Festival = require("../models/Festival.model");
 
 const router = require("express").Router();
 
-
 router.get("/festivals", (req, res, next) => {
   Festival.find()
-  .then((festivalsInDB) => {
-    console.log(festivalsInDB)
-      res.render("festivals/list", {festivalsInDB})
-  })
-  .catch(err => {
-      console.log("Error getting festivals from DB", err)
-  })
-})
+    .then((festivalsInDB) => {
+      console.log(festivalsInDB);
+      res.render("festivals/list", { festivalsInDB });
+    })
+    .catch((err) => {
+      console.log("Error getting festivals from DB", err);
+    });
+});
 
 //   router.get("/profile/myfestival", (req, res) => {
 //     res.render("festivals/myfestival");
 // });
 
-    router.get("/profile/myfestival", (req, res) => {
-    console.log(req.session.user)
-    Festival.findOne({submitter: req.session.user._id })
-    .then((festivalFromDB)=>{
-      console.log(festivalFromDB)
-        if(festivalFromDB){
-          res.render("festivals/myfestival", {festivalFromDB});
-        } 
+router.get("/profile/myfestival", (req, res) => {
+  Festival.findOne({ submitter: req.session.user._id })
+    .then((festivalFromDB) => {
+      res.render("festivals/myfestival", { festivalFromDB });
     })
-    .catch(err=>{
-      console.log("Error getting my festival from db", err)
-    })
-  });
-
+    .catch((err) => {
+      console.log("Error getting my festival from db", err);
+    });
+});
 
 router.get("/profile/add-my-festival", (req, res) => {
   res.render("festivals/createfestival");
@@ -40,32 +34,29 @@ router.get("/profile/add-my-festival", (req, res) => {
 router.post("/profile/myfestival", (req, res) => {
   const myFestival = {
     title: req.body.title,
-    submitter: req.session.user
-  }
+    submitter: req.session.user,
+  };
   Festival.create(myFestival)
-  .then((myFestival)=>{
-    res.render("festivals/myfestival", {myFestival});
-  })
-  .catch(err=>{
-    console.log("Error creating my festival", err)
-  })
+    .then((myFestival) => {
+      //res.render("festivals/myfestival", { myFestival });
+      res.redirect("/profile/myfestival");
+    })
+    .catch((err) => {
+      console.log("Error creating my festival", err);
+    });
 });
 
-   router.post("/profile/myfestival/remove", (req, res, next)=>{
-    console.log("something")
-   Festival.findOneAndDelete({submitter: req.session.user._id })
-  .then((result)=>{
-    console.log("something2")
-
-    console.log(result)
-    res.render("/profile/myfestival")
-  })
-  .catch(err=>{
-    console.log("Error deleting my festival", err)
-  })
-
-})
-
-
+router.post("/profile/myfestival/remove", (req, res, next) => {
+  console.log("something");
+  Festival.findOneAndDelete({ submitter: req.session.user._id })
+    .then((result) => {
+      console.log("something2");
+      console.log(result);
+      res.render("festivals/myfestival");
+    })
+    .catch((err) => {
+      console.log("Error deleting my festival", err);
+    });
+});
 
 module.exports = router;
