de4610f572d983671c9fd56d9e5f4dfcc7ecbcf0
Gabriel Gomez <ggomez.box@gmail.com>
2022-10-11 12:18:11 +0200

    implement searching festivals

--- models/Festival.model.js
+++ models/Festival.model.js
@@ -1,40 +1,38 @@
 const { Schema, model } = require("mongoose");
 
-
 const festivalSchema = new Schema({
-
-    title: {
-       type: String,
-       required: true
+  title: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  country: String,
+  location: String,
+  format: {
+    type: String,
+  },
+  eventStartDate: Date,
+  eventEndDate: Date,
+  submissionDeadline: Date,
+  image: String,
+  entryFee: Number,
+  acceptedCategories: String,
+  acceptedLength: String,
+  contactPerson: String,
+  contactEmail: String,
+  website: String,
+  poster: String,
+  logo: String,
+  submitter: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+  submittedFilms: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Film",
     },
-    description: String,
-    country: String,
-    location: String,
-    format: {
-        type: String,
-    },
-    eventStartDate: Date,
-    eventEndDate: Date,
-    submissionDeadline: Date,
-    entryFee: Number,
-    acceptedCategories: String,
-    acceptedLength: String,
-    contactPerson: String,
-    contactEmail: String,
-    website: String,
-    poster: String,
-    logo: String,
-    submitter: {
-        type: Schema.Types.ObjectId,
-        ref: "User"
-    },
-    submittedFilms: [{
-        type: Schema.Types.ObjectId,
-        ref: "Film"
-    }]
-})
-
-
-
+  ],
+});
 
 module.exports = model("Festival", festivalSchema);
